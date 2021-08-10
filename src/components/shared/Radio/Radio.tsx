b69557e1618876a5633f9e126674f884eca74f13
Vladyslav <vladyslav.chychykalo@gmail.com>
2021-08-09 17:48:30 -0700

    small update

--- src/components/shared/Radio/Radio.tsx
+++ src/components/shared/Radio/Radio.tsx
@@ -1,10 +1,18 @@
-const Radio = () => {
+import styled from 'styled-components'
+
+const StyledLabel = styled.label``
+
+const StyledInput = styled.input``
+
+const StyledBlock = styled.div``
+
+const Radio = ({text}: {text: string}) => {
   return (
-    <label htmlFor="myRadioId">
-      <input type="radio" name="myRadioFromField" id="myRadioId" />
-      <div></div>
-      Text
-    </label>
+    <StyledLabel htmlFor="customRadioId">
+      <StyledInput type="radio" id="customRadioId" name="gender" />
+      <StyledBlock></StyledBlock>
+      {text}
+    </StyledLabel>
   );
 };
 
